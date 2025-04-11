653ff1a22409f63e277d8ed6d012393d501379a9
Peter Li <pl.climbs@gmail.com>
2025-04-11 01:42:09 -0700

    Add initial docker-compose and helper script for provisioning and fetching certs from LetsEncrypt

---
+++ fetch-ssl-certs.sh
@@ -0,0 +1,83 @@
+#!/usr/bin/env bash
+
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 \$DOMAIN_NAME \$SERVICE_PORT"
+    exit 1
+fi
+
+DOMAIN_NAME=$1
+SERVICE_PORT=$2
+
+# Generate configs for challenge server
+cat << EOF > nginx-challenge.conf
+events {
+  worker_connections 16;
+}
+
+http {
+  server {
+    listen 80;
+    server_name ${DOMAIN_NAME};
+
+    location /.well-known/acme-challenge/ {
+      root /var/www/certbot;
+    }
+
+    location / {
+      deny all;
+    }
+  }
+}
+EOF
+
+# Run challenge server in background just for certbot
+docker compose up --detach challenge
+docker compose run --rm \
+    certbot certonly --webroot \
+    --webroot-path /var/www/certbot/ \
+    -d $DOMAIN_NAME \
+    || echo "Failed to obtain or renew ssl certs"
+docker compose down challenge
+
+# Cleanup generated files
+rm -f nginx-challenge.conf
+
+# Generate new nginx config
+cat << EOF > nginx.conf
+events {
+  worker_connections 512;
+  multi_accept on;
+  use epoll;
+}
+http {
+  server {
+    listen 80;
+    server_name ${DOMAIN_NAME};
+
+    location / {
+      proxy_pass http://${DOMAIN_NAME}:${SERVICE_PORT};
+      proxy_set_header Host \$host;
+      proxy_set_header X-Real-IP \$remote_addr;
+      proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+      proxy_set_header X-Forwarded-Proto \$scheme;
+    }
+  }
+
+  server {
+    listen 443 default_server ssl http2;
+    listen [::]:443 ssl http2;
+    server_name ${DOMAIN_NAME};
+
+    ssl_certificate     /etc/nginx/ssl/live/${DOMAIN_NAME}/fullchain.pem;
+    ssl_certificate_key /etc/nginx/ssl/live/${DOMAIN_NAME}/privkey.pem;
+
+    location / {
+      proxy_pass http://${DOMAIN_NAME}:${SERVICE_PORT};
+      proxy_set_header Host \$host;
+      proxy_set_header X-Real-IP \$remote_addr;
+      proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+      proxy_set_header X-Forwarded-Proto \$scheme;
+    }
+  }
+}
+EOF
